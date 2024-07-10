3c82a3d1e4a787a62339b6a3f04a35cca1b59f35
rochdir <Ranarochdi2003@gmail.com>
2024-07-10 12:37:40 +0200

    showcase products in the form last page

---
+++ .angular/cache/17.0.7/vite/deps/firebase_compat_app.js
@@ -0,0 +1,9 @@
+import "./chunk-VCWED7VY.js";
+import {
+  firebase
+} from "./chunk-I4O6BFVM.js";
+import "./chunk-EQU7RARR.js";
+export {
+  firebase as default
+};
+//# sourceMappingURL=firebase_compat_app.js.map
